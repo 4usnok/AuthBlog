7dd6af4f29f2717ad81e45fd86c79305cd19061c
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-06-02 01:11:42 +0300

    part 1: registration over

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-import sqlite3
-
 from src.database.crud import ClassFromCrud
 
 class_obj = ClassFromCrud()
 print(class_obj.add_to_db())
-
-input_select = input("Посмотреть содержимое БД?: ")
-if input_select.lower() == "Да".lower():
-    connection = sqlite3.connect('data/accounts.db')
-    cursor = connection.cursor()
-    # Выбираем всех пользователей
-    cursor.execute('SELECT * FROM accounts')
-    users = cursor.fetchall()
-
-    # Выводим результаты
-    for user in users:
-        print(user)
-
-    # Закрываем соединение
-    connection.close()
-elif input_select.lower() == "Нет".lower():
-    pass
--- src/database/crud.py
+++ src/database/crud.py
@@ -22,13 +22,27 @@ class ClassFromCrud:
         login = input("login: ")
         password = input("password: ")
 
-        # хеширование пароля
-        hashed_password = hashing_password(password)
+        # проверяем, есть ли уже такой пользователь в БД
+        existing_login_mail = (
+                db.query(users).filter((users.mail == mail)).first()
+                or db.query(users).filter((users.login == login)).first()
+        )
 
-        add_in_db = users(mail=mail, login=login, password=hashed_password)
-        db.add(add_in_db)     # добавляем в бд
-        print("Регистрация успешно выполнена!")
-        return db.commit()     # сохраняем изменения
+        # проверка на наличие повторяющихся данных(почты и логина)
+        if existing_login_mail:
+            print("Пользователь с таким login или mail уже зарегистрирован.")
+        # проверка на корректность структуры электронной почты
+        elif '@' not in mail or '.' not in mail.split('@')[-1]:
+            print("Ошибка: Email должен быть вида name@domain.com")
+        else:
+            # хеширование пароля
+            hashed_password = hashing_password(password)
+            add_in_db = users(mail=mail, login=login, password=hashed_password)
+            # добавляем в бд
+            db.add(add_in_db)
+            print("Регистрация успешно выполнена!")
+            # сохраняем изменения
+        return db.commit()
 
 # приложение, которое ничего не делает
 app = FastAPI()
